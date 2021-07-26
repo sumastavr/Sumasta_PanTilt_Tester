7809e50fbbfa8a41fd886e94cc5a6b1b099e8f17
sumastavr <pamungkas@sumasta.tech>
2021-07-26 20:33:18 +0200

    Stable release pan and tilt slider only

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,12 @@
 #include <WiFi.h>
 #include <ESP32Servo.h>
 
-Servo myservo;  // create servo object to control a servo
+Servo myservoPan;  // create servo object to control a servo pan
+Servo myservoTilt;  // create servo object to control a servo pan
 
 // Servo GPIO pin
-static const int servoPin = 18;
+static const int servoPanPin = 23;
+static const int servoTiltPin = 32;
 
 // Network credentials
 const char* ssid     = "Sumasta-PanTilt";
@@ -22,6 +24,11 @@ String header;
 String valueString = String(5);
 int pos1 = 0;
 int pos2 = 0;
+
+int currentPan=5;
+int currentTilt=5;
+
+int moveSpeed=20;
 
 // Current time
 unsigned long currentTime = millis();
@@ -39,12 +46,14 @@ void setup() {
   ESP32PWM::allocateTimer(3);
   
   // Set servo PWM frequency to 50Hz
-  myservo.setPeriodHertz(50);
+  myservoPan.setPeriodHertz(50);
+  myservoTilt.setPeriodHertz(60);
   
   // Attach to servo and define minimum and maximum positions
   // Modify as required
-  myservo.attach(servoPin,500, 2400);
-  
+  myservoPan.attach(servoPanPin,500, 2400);
+  myservoTilt.attach(servoTiltPin,500, 2400);
+
   // Start serial
   Serial.begin(115200);
   delay(1000);
@@ -84,7 +93,7 @@ void loop(){
       currentTime = millis();
       if (client.available()) {             // if there's bytes to read from the client,
         char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
+        //Serial.write(c);                    // print it out the serial monitor
         header += c;
         if (c == '\n') {                    // if the byte is a newline character
           // if the current line is blank, you got two newline characters in a row.
@@ -115,14 +124,16 @@ void loop(){
             // Page title
             client.println("</head><body style=\"background-color:#70cfff;\"><h1 style=\"color:#ff3410;\">Pan-Tilt Controller</h1>");
             
-            // Position display
+            // Sliders and Position display
             client.println("<h2 style=\"color:#ffffff;\">Pan: <span id=\"servoPos\"></span>&#176;</h2>"); 
-                    
-            // Slider control
-            client.println("<input type=\"range\" min=\"-150\" max=\"150\" class=\"slider\" id=\"servoSlider\" onchange=\"servo(this.value)\" value=\""+valueString+"\"/>");            
+            client.println("<input type=\"range\" min=\"0\" max=\"180\" class=\"slider\" id=\"servoSlider\" onchange=\"servo(this.value)\" value=\""+valueString+"\"/>");            
             client.println("<h2 style=\"color:#ffffff;\">Tilt: <span id=\"servoPos2\"></span>&#176;</h2>"); 
-            client.println("<input type=\"range\" min=\"-45\" max=\"45\" class=\"slider\" id=\"servoSlider2\" onchange=\"servo2(this.value)\" value=\""+valueString+"\"/>");
+            client.println("<input type=\"range\" min=\"0\" max=\"180\" class=\"slider\" id=\"servoSlider2\" onchange=\"servo2(this.value)\" value=\""+valueString+"\"/>");
             
+            //client.println("<h2 style=\"color:#ffffff;\">Speed: <span id=\"speedPos\"></span></h2>"); 
+            //client.println("<input type=\"range\" min=\"0\" max=\"100\" class=\"slider\" id=\"speedSlider\" onchange=\"speed(this.value)\" value=\""+valueString+"\"/>");
+            
+
             // Javascript
             client.println("<script>var slider = document.getElementById(\"servoSlider\");");            
             client.println("var servoP = document.getElementById(\"servoPos\"); servoP.innerHTML = slider.value;");            
@@ -131,12 +142,19 @@ void loop(){
             client.println("var slider2 = document.getElementById(\"servoSlider2\");");
             client.println("var servoP2 = document.getElementById(\"servoPos2\"); servoP2.innerHTML = slider2.value;");
             client.println("slider2.oninput = function() { slider2.value = this.value; servoP2.innerHTML = this.value; }");
+/*
+            client.println("var slider3 = document.getElementById(\"speedSlider\");");
+            client.println("var speedSlider = document.getElementById(\"speedPos\"); speedSlider.innerHTML = slider3.value;");
+            client.println("slider3.oninput = function() { slider3.value = this.value; speedSlider.innerHTML = this.value; }");
 
+            client.println("$.ajaxSetup({timeout:1000}); function speed(pos) { ");
+            client.println("$.get(\"/?value=\" +\"S\" +pos + \"&\"); {Connection: close};}");
+*/
             client.println("$.ajaxSetup({timeout:1000}); function servo(pos) { ");
-            client.println("$.get(\"/?value=\" + pos + \"&\"); {Connection: close};}");
+            client.println("$.get(\"/?value=\" +\"P\"+pos + \"&\"); {Connection: close};}");
 
             client.println("function servo2(pos) { ");
-            client.println("$.get(\"/?value=\" +\"X\"+pos + \"&\"); {Connection: close};}</script>");
+            client.println("$.get(\"/?value=\" +\"T\"+pos + \"&\"); {Connection: close};}</script>");
             
             // End page
             client.println("</body></html>");     
@@ -153,15 +171,51 @@ void loop(){
               Serial.print("RawVal =");
               Serial.println(valueString); 
 
-              if(valueString.charAt(0)=='X'){
+              if(valueString.charAt(0)=='T'){
                 valueString=valueString.substring(1);
                 Serial.print("TILT:");
                 Serial.println(valueString.toInt());
-                //myservo.write(valueString.toInt());
-              }else{
+
+                int tiltValue=valueString.toInt();
+
+                if(tiltValue>currentTilt){
+                  for(int i=currentTilt;i<tiltValue;i++){
+                    myservoTilt.write(i);
+                    delay(moveSpeed);
+                    currentTilt=tiltValue;    
+                  }      
+                }else{
+                  for(int i=currentTilt;i>tiltValue;i--){
+                    myservoTilt.write(i);
+                    delay(moveSpeed);
+                    currentTilt=tiltValue;    
+                  }
+                }
+              }else if(valueString.charAt(0)=='P'){
+                valueString=valueString.substring(1);
                 Serial.print("PAN:");
                 Serial.println(valueString.toInt());
-                //myservo.write(valueString.toInt());
+                
+                int panValue=valueString.toInt();
+
+                if(panValue>currentPan){
+                  for(int i=currentPan;i<panValue;i++){
+                    myservoPan.write(i);
+                    delay(moveSpeed);
+                    currentPan=panValue;    
+                  }      
+                }else{
+                  for(int i=currentPan;i>panValue;i--){
+                    myservoPan.write(i);
+                    delay(moveSpeed);
+                    currentPan=panValue;    
+                  }
+                }              
+              }else if(valueString.charAt(0)=='S'){
+                valueString=valueString.substring(1);
+                Serial.print("SPEED:");
+                moveSpeed=valueString.toInt();
+                Serial.println(moveSpeed);
               }
 
             }         
